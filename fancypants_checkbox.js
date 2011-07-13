cecb87b99d76a42d14771b9df1b335fcb3a487fb
Stuart <stuart.leigh83@gmail.com>
2011-07-13 15:36:03 +0100

    fixed the problem with pre-checked checkboxes screwing everything up

--- fancypants_checkbox.js
+++ fancypants_checkbox.js
@@ -1,33 +1,38 @@
 $.fn.fancypants_checkbox = function () {
   return this.each(function(){
     $(this).after('<div class="fp_cb_holder"><img src="toggle.png" alt="fancypants checkbox toggle" class="fp_cb_toggle" width="30" height="21" /><div class="fp_cb_inner"><div class="fp_cb_slider"><span class="fp_cb_yes" style="display:none;">yep</span><span class="fp_cb_no">nope</span></div></div></div>');
-    var $toggle = $(this).next().find('.fp_cb_toggle')
-    $(this).next().toggle(function(){
-		$toggle.animate({
-			left: $(this).width() - $toggle.width() + 7
-		}, 100);
-		$toggle.next().animate({backgroundColor: '#a8ff00'}, 100);
-		$toggle.parent().find('.fp_cb_no').fadeOut(100)
-		$toggle.parent().find('.fp_cb_yes').fadeIn(100)
-		$(this).prev().click();
-	}, function(){
-		$toggle = $(this).find('.fp_cb_toggle')
-		$toggle.animate({
-			left: 6
-		}, 100);
-		$toggle.next().animate({backgroundColor: '#ed1c24'}, 100)
-		$toggle.parent().find('.fp_cb_yes').hide()
-		$toggle.parent().find('.fp_cb_no').fadeIn(100)
-		$(this).prev().click();
-	});
-	if($(this).is(':checked')){
-		/*$toggle.css({
-			left: $(this).next().width() - $toggle.width() + 7
-		});
-		$toggle.next().css({backgroundColor: '#a8ff00'});
-		$toggle.parent().find('.fp_cb_no').hide()
-		$toggle.parent().find('.fp_cb_yes').show()*/
-		$toggle.click();
-	}
+    var $toggle = $(this).next().find('.fp_cb_toggle'), $toggle_box = $(this).next();
+    if($(this).is(':checked')){
+      $toggle_box.data('is_checked', true);
+      $toggle.css({
+        left: $(this).next().width() - $toggle.width() + 7
+      });
+      $toggle.next().css({backgroundColor: '#a8ff00'});
+      $toggle_box.find('.fp_cb_no').hide()
+      $toggle_box.find('.fp_cb_yes').show()
+    } else {
+      $toggle_box.data('is_checked', false);
+    }
+    $toggle_box.click(function(){
+      if($(this).data('is_checked')){
+        $toggle.animate({
+          left: 6
+        }, 100);
+        $toggle.next().animate({backgroundColor: '#ed1c24'}, 100);
+        $toggle_box.find('.fp_cb_yes').fadeOut(100);
+        $toggle_box.find('.fp_cb_no').fadeIn(100);
+        $(this).data('is_checked', false);
+        $(this).prev().click();
+      } else {
+        $toggle.animate({
+          left: $(this).width() - $toggle.width() + 7
+        }, 100);
+        $toggle.next().animate({backgroundColor: '#a8ff00'}, 100);
+        $toggle_box.find('.fp_cb_no').fadeOut(100);
+        $toggle_box.find('.fp_cb_yes').fadeIn(100);
+        $(this).data('is_checked', true);
+        $(this).prev().click();
+      }
+    });
   });
 };
